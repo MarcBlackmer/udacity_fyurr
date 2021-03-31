53eb98b75779a2796f50fa695189eaff2afcc9d9
Marc Blackmer <marc.blackmer@1nterrupt.com>
2021-03-30 17:19:37 -0700

    Added CSRF protection

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_moment import Moment
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 #----------------------------------------------------------------------------#
 # Models.
